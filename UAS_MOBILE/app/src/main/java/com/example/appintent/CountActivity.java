dda616727135fcd90c732ae9704a2fd80514752b
Yoga Pratama <115678171+yogafrtm25@users.noreply.github.com>
2024-01-14 14:30:22 +0700

    Update CountActivity.java

--- UAS_MOBILE/app/src/main/java/com/example/appintent/CountActivity.java
+++ UAS_MOBILE/app/src/main/java/com/example/appintent/CountActivity.java
@@ -1,37 +1,128 @@
 package com.example.appintent;
 
+
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class CountActivity extends AppCompatActivity {
-    private int nCount = 0;
+    private TextView show_count;
+    private int count = 1;
+    private long fibNMinus1 = 1;
+    private long fibNMinus2 = 1;
+    private int limit = -1; // Inisialisasi limit dengan nilai default
 
-    private TextView nShowCount;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count);
-        nShowCount = findViewById(R.id.show_count);
+
+        show_count = findViewById(R.id.show_count);
     }
 
-    public void showToast(View view){
-        Toast toast = Toast.makeText(this, "Menghitung Bilangan",
-                Toast.LENGTH_SHORT);
-        toast.show();
+    public void countUp(View view) {
+        if (count == 0) {
+            show_count.setText("0");
+        }
+        else if (count == 1) {
+            show_count.setText("1");
+        }
+        else {
+            if (limit != -1 && count > limit) {
+                // Jika count melebihi limit, atur ulang perhitungan
+                count = 0;
+                fibNMinus1 = 1;
+                fibNMinus2 = 0;
+                show_count.setText(getString(R.string.count_initial_value));
+            }
+            else {
+                long fibCurrent = fibNMinus1 + fibNMinus2;
+                fibNMinus2 = fibNMinus1;
+                fibNMinus1 = fibCurrent;
+
+                //Mengatur warna teks berdasarkan angka Fibonacci
+                int colorResId = R.color.colorAccent; // Warna Default
+                switch (count % 11) {
+                    case 1:
+                        colorResId = R.color.colorAccent; // Warna Pink Tua
+                        break;
+                    case 2:
+                        colorResId = R.color.hijaumuda; // Warna Hijau Muda
+                        break;
+                    case 3:
+                        colorResId = R.color.purple; // Warna Ungu
+                        break;
+                    case 4:
+                        colorResId = R.color.salem; // Warna Salem
+                        break;
+                    case 5:
+                        colorResId = R.color.birumuda; // Warna Biru Muda
+                        break;
+                    case 6 :
+                        colorResId = R.color.kuning; // Warna Kuning
+                        break;
+                    case 7:
+                        colorResId = R.color.hijau; // Warna Hijau
+                        break;
+                    case 8:
+                        colorResId = R.color.cream; // Warna Cream
+                        break;
+                    case 9:
+                        colorResId = R.color.biru; // Warna Biru
+                        break;
+                    case 10:
+                        colorResId = R.color.orange; // Warna Orange
+                        break;
+                }
+                show_count.setTextColor(getResources().getColor(colorResId));
+                show_count.setText(String.valueOf(fibCurrent));
+            }
+        }
+
+        count++;
     }
 
-    @SuppressLint("SetTextI18n")
-    public void countUp(View view){
-        nCount++;
-        if (nShowCount != null)
-            nShowCount.setText(Integer.toString(nCount));
+    public void back1(View view) {
+        count = 1;
+        fibNMinus1 = 1;
+        fibNMinus2 = 0;
+        show_count.setText(getString(R.string.count_initial_value));
+    }
+
+    public void setLimit(View view) {
+        // Create and display a dialog to set the limit
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Set Limit");
+
+        final EditText input = new EditText(this);
+        input.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Get the limit from the input and set it for calculations
+                String limitStr = input.getText().toString();
+                limit = Integer.parseInt(limitStr);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
